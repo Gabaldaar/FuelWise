b443998d9543ab4af523b31fae9fb2832fd579f6
Gabriel Aldazabal <gab.aldazabal@gmail.com>
2025-11-05 12:30:28 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1769907004-5fad3",
+  "appId": "1:55628356119:web:caa7e188484dd1474cda52",
+  "apiKey": "AIzaSyDeFZbUvS0NbnidsrJ3MTs0ddEMvgKgfd4",
+  "authDomain": "studio-1769907004-5fad3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "55628356119"
+};
